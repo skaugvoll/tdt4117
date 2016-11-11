2cc0aad07ea8b8c3b8eaa240894f8d9cf6cfeba1
Christoffer Skar Lofsberg <chris.lofsberg@gmail.com>
2016-11-11 10:39:00 +0100

    Add indexing

--- Assignment 4/ir4-sample/src/no/ntnu/idi/ir/MyDocument.java
+++ Assignment 4/ir4-sample/src/no/ntnu/idi/ir/MyDocument.java
@@ -2,11 +2,10 @@ package no.ntnu.idi.ir;
 
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.IndexableField;
 
 /*
  * The task is to update the given `MyDocument' class (i.e implement the 'Document(File F)' method) to index the following fields per document:
@@ -16,18 +15,18 @@ import org.apache.lucene.document.TextField;
  * - subject, the subject of the e-mail
  * - contents, the actual e-mail contents
  *
- * All `from', `subject', and `contents' should be searchable, i.e. store their re-spective term vectors. 
+ * All `from', `subject', and `contents' should be searchable, i.e. store their re-spective term vectors.
  * Look at the given `NewsDocument' class that reads a text file and has better methods for `from', `subject', and `contents'.
  *
  * =============
  *
- * Lucene official documentation: 
+ * Lucene official documentation:
  * http://lucene.apache.org/core/4_10_1/index.html
  *
- * Useful resources: 
+ * Useful resources:
  *   http://oak.cs.ucla.edu/cs144/projects/lucene/
  *   http://lingpipe-blog.com/2014/03/08/lucene-4-essentials-for-text-search-and-indexing/
- * 
+ *
  */
 
 
@@ -42,6 +41,10 @@ public class MyDocument{
 		NewsDocument newsDocument = new NewsDocument(f);
 
 		//TODO: create structured lucene document
+		doc.add(new StringField("id", newsDocument.getId(), Field.Store.YES));
+		doc.add(new TextField("from", newsDocument.getFrom(), Field.Store.YES));
+		doc.add(new TextField("subject", newsDocument.getSubject(), Field.Store.YES));
+		doc.add(new TextField("contents", newsDocument.getContent(), Field.Store.YES));
 
 		return doc;
 	}
